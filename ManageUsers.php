77de6135e1f318533915a3224e17b08df5f8ea30
Biratheeban <199211204+ms25913156-Tharmmendra@users.noreply.github.com>
2025-05-11 13:37:34 +0530

    Update ManageUsers.php

--- ManageUsers.php
+++ ManageUsers.php
@@ -1,10 +1,10 @@
 <?php
 require_once 'db.php';
 
-if( $_SESSION['role']!= 'admin'){
-    header("Location: index.php");
-    exit();
-}
+// if( $_SESSION['role']!= 'admin'){
+//     header("Location: index.php");
+//     exit();
+// }
 
 
 
